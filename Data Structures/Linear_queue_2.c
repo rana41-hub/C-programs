a728de9f3bba7d4d390f04ef841c595b3847d07d
rana41-hub <pratapranak41@gmail.com>
2025-11-11 23:19:22 +0530

    Some more modifications

--- Data Structures/Linear_queue_2.c
+++ Data Structures/Linear_queue_2.c
@@ -1,91 +1,78 @@
-#include<stdio.h>
+#include <stdio.h>
 #define MAXSIZE 10
 
-int queue[MAXSIZE] , front = -1 , rear = -1 ;
-void insert() , delete() , display();
+int queue[MAXSIZE];
+int front = -1, rear = -1;
+
+void insert();
+void delete();
+void display();
 
 int main() {
+    int choice;
+    do {
+        printf("\nLinear Queue Implementation\n");
+        printf("----------------------------\n");
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("----------------------------\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-  int choice;
+        switch (choice) {
+            case 1: insert(); break;
+            case 2: delete(); break;
+            case 3: display(); break;
+            case 4: printf("Exiting...\n"); break;
+            default: printf("Invalid Choice\n");
+        }
+    } while (choice != 4);
 
-  do {
-
-    printf("Linear Queue Implementation\n");
-    printf("------------------------\n");
-    printf("\n1.Insert \n2.Delete \n3.Display\n");
-    printf("------------------------\n");
-    printf("Enter your choice : ");
-    scanf("%d", &choice);
-    switch(choice) {
-      case 1 : {
-        insert();
-        break;
-      }
-      case 2 : {
-        delete();
-        break;
-      }
-      case 3 : {
-        display();
-        break;
-      }
-      default : {
-        printf("Invalid Choice");
-      }
-    }
-  }
-
-    while(choice != 4);
     return 0;
 }
 
 void insert() {
-
-  if(rear == MAXSIZE - 1) {
-
-    printf("Queue Overflow\n");
-
-  } 
-
-  else {
-
     int n;
-    printf("Enter the element to insert : ");
-    scanf("%d",&n);
+    if (rear == MAXSIZE - 1) {
+        printf("Queue Overflow\n");
+        return;
+    }
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &n);
+
+    if (front == -1) // first insertion
+        front = 0;
     rear++;
-    front++;
     queue[rear] = n;
-  }
-  
+
+    printf("%d inserted into the queue\n", n);
 }
 
 void delete() {
+    if (front == -1 || front > rear) {
+        printf("Queue Underflow / Empty\n");
+        return;
+    }
 
-  int deletedElement;
+    printf("Deleted element: %d\n", queue[front]);
+    front++;
 
-  if(rear == -1) {
-
-    printf("Queue is empty\n");
-
-  }
-  else {
-
-    deletedElement = queue[front];
-    front--;
-    printf("Deleted element is : %d\n",deletedElement);
-
-  }
+    if (front > rear) // reset queue if empty
+        front = rear = -1;
 }
 
 void display() {
-
-  if (rear == -1) {
-    printf("Queue is empty\n");
-  } 
-  else {
-    printf("Elements in the queue are : \n");
-    for(int i = front; i > -1 ; i--) {
-      printf("%d\n", queue[i]);
+    if (front == -1) {
+        printf("Queue is empty\n");
+        return;
     }
-  }
+
+    printf("Elements in the queue:\n");
+    for (int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+    printf("\n");
 }
